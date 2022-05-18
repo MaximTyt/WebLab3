a8f6b0eb551cd9604b7ff128a0c5fd8ffb8a4960
Maxim Tyt <mr.lexa.yan@mail.ru>
2022-05-18 18:58:54 +0300

    добавил проверку на наличие пользователей в бд

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ app.use("/image", express.static('image'))
 app.use("/script", express.static('script'))
 app.use("/css", express.static('css'))
 
+var Datastore = require('nedb');
+var users = new Datastore({ filename: 'users.db', autoload: true });
+const admin = {
+    login: 'admin',
+    password: 'd033e22ae348aeb5660fc2140aec35850c4da997'
+};
+users.ensureIndex({ fieldName: 'login', unique: true });
+users.insert(admin);
+
 app.get('/v1/authorization', function (req, res) {
     res.sendFile(__dirname + '/views/authorization.html')
 });
@@ -45,10 +54,15 @@ app.post("/v1/authorization",
         let sha1 = crypto.createHash('sha1')
         let hash = sha1.update(req.body.password).digest('hex')
         req.body.password = hash
-        const token = jwt.sign({ id: 1, role: "admin" }, secret);
-        return res
-            .status(200)
-            .send({"access_token":token})
+        users.findOne({login: req.body.login},function (err,doc){
+        if(doc.password === req.body.password){
+            const token = jwt.sign({ id: 1, role: "admin" }, secret);
+            return res
+                .status(200)
+                .send({"access_token":token})
+        }
+        });
+        return res.status(403).sendFile(__dirname + '/views/403.html');
     });
 
 
